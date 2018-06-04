25461bb430c0d8e5875b09072e3986492e2c7f5e
Peter MOUEZA <mouezapeter@gmail.com>
2018-06-05 00:43:43 +0200

    start

---
+++ spring-data/src/main/java/homniserv/moueza/spring_data/App.java
@@ -0,0 +1,13 @@
+package homniserv.moueza.spring_data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
