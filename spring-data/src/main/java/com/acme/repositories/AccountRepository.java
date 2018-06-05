1f4b2b29d4fee5e9b09937729d063fb46924a89d
Peter MOUEZA <mouezapeter@gmail.com>
2018-06-06 00:14:33 +0200

    no red OK

--- spring-data/src/main/java/com/acme/repositories/AccountRepository.java
+++ spring-data/src/main/java/com/acme/repositories/AccountRepository.java
@@ -3,11 +3,13 @@ package com.acme.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import entity.Account;
 import entity.Customer;
 
 /** https://spring.io/blog/2011/02/10/getting-started-with-spring-data-jpa/ */
+@Transactional(readOnly = true)
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 	List<Account> findByCustomer(Customer customer);
